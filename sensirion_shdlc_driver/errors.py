14f39e14764238ea25009acda411804184580259
Urban Bruhin <urban.bruhin@sensirion.com>
2019-01-29 08:29:30 +0100

    ShdlcResponseError: Fix Python 2 compatibility issues
    Data type of some variables were not as expected on Python 2.

--- sensirion_shdlc_driver/errors.py
+++ sensirion_shdlc_driver/errors.py
@@ -41,9 +41,10 @@ class ShdlcResponseError(ShdlcError):
         )
         self._received_data = received_data
         if self._received_data is not None:
-            self._received_data = bytes(bytearray(received_data))
+            received_data_bytearray = bytearray(self._received_data)
+            self._received_data = bytes(received_data_bytearray)
             log.debug("Invalid SHDLC response raw data: [{}]".format(
-                ", ".join(["0x%.2X" % i for i in self._received_data])))
+                ", ".join(["0x%.2X" % i for i in received_data_bytearray])))
 
     @property
     def received_data(self):
@@ -73,7 +74,7 @@ class ShdlcDeviceError(ShdlcError):
             "SHDLC device returned error code {}: {}".format(code, message)
         )
         self._error_code = code
-        self._error_message = message
+        self._error_message = str(message)
 
     @property
     def error_code(self):
