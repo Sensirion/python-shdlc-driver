8c6c7ca70052ea59b8ca71e47e63aae2e5c3d380
qfisch <quentin.fisch@sensirion.com>
2026-03-19 15:02:37 +0100

    Replaced deprecated pkg_resources

--- docs/conf.py
+++ docs/conf.py
@@ -11,20 +11,19 @@ import sys
 import sphinx.ext.autodoc
 from datetime import datetime
 
-import pkg_resources
+import importlib.metadata as metadata
 
 # Add project directory such that sphinx can detect the package.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # -- Project information -----------------------------------------------------
-distribution = pkg_resources.get_distribution('sensirion_shdlc_driver')
+distribution = metadata.distribution('sensirion_shdlc_driver')
 
-project = distribution.project_name
+project = distribution.name
 copyright = u'{} Sensirion AG, Switzerland'.format(datetime.now().year)
 author = 'Sensirion AG'
 version = distribution.version
 release = distribution.version
-
 
 # -- General configuration ---------------------------------------------------
 
